9050603c64da7e94fe4e0b8c522d0a391bc268b9
wulin_wu <wu_wulin@qq.com>
2023-04-05 23:21:20 +0800

    feat[Ctrl-C]:handle Ctrl-C signal
    1. bring ctrlc crate to handle Ctrl-C signal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+
 fn main() {
-    println!("Hello, world!");
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    })
+    .expect("Error setting Ctrl-C handler");
+
+    while running.load(Ordering::SeqCst) {
+        //do things
+    }
+
+    // reset dwm statusbar
+    // send: xsetroot -name ''
 }
