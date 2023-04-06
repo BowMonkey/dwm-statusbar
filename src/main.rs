2181591e57dac33e3166d8438132af9cb27accf8
wulin_wu <wu_wulin@qq.com>
2023-04-06 23:28:00 +0800

    feat[name-lock]:singal run
    1. use name-lock prevent double run
    
    ! alarm: current code cannot compile, should add error handle

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 
-fn main() {
+fn main() -> Result<(), Errors> {
+    // prevent double open
+    let lock = named_lock::NamedLock::create("dwm-statusbar")?;
+    let _guard = lock.try_lock();
+    match _guard {
+        Ok(_) => {
+            // first run
+        }
+        Err(e) => match e {
+            named_lock::Error::WouldBlock => {
+                // double open, exit
+                return Ok(());
+            }
+            _ => {
+                println!("Other Error!");
+                return Err(Errors::DoubleOpenError(e));
+            }
+        },
+    }
+
     let running = Arc::new(AtomicBool::new(true));
     let r = running.clone();
 
@@ -14,6 +33,7 @@ fn main() {
         //do things
     }
 
+    Ok(())
     // reset dwm statusbar
     // send: xsetroot -name ''
 }
