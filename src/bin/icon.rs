654fe931392b3b70a16518681b7e7ec269f7903c
wulin_wu <wu_wulin@qq.com>
2023-04-15 10:54:56 +0800

    refactor[icon]:add icon module function

--- src/bin/icon.rs
+++ src/bin/icon.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::process::Command;
 
-use utillib::util::{calc_notify_id, send_dwm, send_notify};
+use utillib::util::{calc_notify_id, get_proc_info, send_dwm, send_notify};
 
 fn main() {
     let args_vec: Vec<String> = env::args().collect();
@@ -34,27 +34,73 @@ fn main() {
         }
     }
 
-    print!("{}", create_dwm_msg(""));
+    print!(
+        "{}",
+        create_dwm_msg("icon".to_string(), "".to_string(), "".to_string())
+    );
 }
 
-fn left_click() {}
+use std::collections::HashMap;
+fn call_rofi() {
+    let mut cmds: HashMap<String, String> = HashMap::new();
+    cmds.insert("󰍃 Logout".to_string(), "logout".to_string());
+    cmds.insert("⏻ Shutdown".to_string(), "shutdown".to_string());
+    cmds.insert(" Reboot".to_string(), "reboot".to_string());
+    cmds.insert(" Lock".to_string(), "lock".to_string());
+
+    let mut cmd_str = String::from("echo $(echo -e '");
+    for key in cmds.keys() {
+        cmd_str += key;
+        cmd_str += "\\n";
+    }
+    cmd_str = cmd_str[0..cmd_str.len() - 2].to_string();
+    cmd_str += "' | rofi -dmenu -window-title Power)";
+    println!("cmd_str for rofi:{}", cmd_str);
+    let selected = get_proc_info(&cmd_str, 20);
+    if let Some(func) = cmds.get(&selected) {
+        match func.as_str() {
+            "logout" => do_logout(),
+            "shutdown" => do_shutdown(),
+            "reboot" => do_reboot(),
+            "lock" => do_lock(),
+            _ => {}
+        }
+    }
+}
+fn left_click() {
+    call_rofi()
+}
 fn right_click() {}
 fn middle_click() {}
 fn up_roll() {}
 fn down_rol() {}
 
-fn create_dwm_msg(s: &str) -> String {
-    let title = "^s".to_string() + "icon" + "^";
+fn create_dwm_msg(title: String, icon: String, text: String) -> String {
+    let title = "^s".to_string() + &title + "^";
     let ic_fg_color = "^c".to_string() + "#1e222a" + "^";
-    let ic_bg_color = "^b".to_string() + "#A3BE8C" + "^";
-    let ic = "".to_string();
+    let ic_bg_color = "^b".to_string() + "#81A1C1" + "^";
+    let ic = icon;
     let tx_fg_color = "^c".to_string() + "#1e222a" + "^";
-    let tx_bg_color = "^b".to_string() + "#A3BE8C" + "^";
-    let tx = s.to_string();
+    let tx_bg_color = "^b".to_string() + "#81A1C1" + "^";
+    let tx = text;
 
-    title + &ic_fg_color + &ic_bg_color + &ic + &tx_fg_color + &tx_bg_color + &tx
+    title + &ic_fg_color + &ic_bg_color + &ic + &tx_fg_color + &tx_bg_color + tx.trim_end()
 }
 
-pub fn call_proc(cmd: &str) {
+fn call_proc(cmd: &str) {
     Command::new("sh").arg("-c").arg(cmd).spawn().unwrap();
+}
+
+fn do_logout() {
+    call_proc("rm /tmp/dwm_avoid_repeat_auto_start.lock");
+    call_proc("pkill -KILL -u coco")
+}
+fn do_shutdown() {
+    call_proc("shutdown -P now")
+}
+fn do_reboot() {
+    call_proc("reboot")
+}
+fn do_lock() {
+    call_proc("slock")
 }
